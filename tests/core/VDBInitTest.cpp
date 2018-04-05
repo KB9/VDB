b549bab03d418f1421fa49a0971aa82798442d43
KB9 <kavanbickerstaff@googlemail.com>
2018-04-05 23:53:23 +0100

    Added Catch2 testing suite

---
+++ tests/core/VDBInitTest.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+
+#include "vdb.hpp"
+
+TEST_CASE("VDB initialization handling")
+{
+	VDB vdb;
+
+	REQUIRE(vdb.isInitialized() == false);
+
+	SECTION("VDB initialization handles an invalid file path")
+	{
+		bool success = vdb.init("invalid_file_path");
+
+		REQUIRE(success == false);
+		REQUIRE(vdb.isInitialized() == false);
+	}
+
+	SECTION("VDB initialization initializes successfully with valid file path")
+	{
+		bool success = vdb.init("data/hello_world");
+
+		REQUIRE(success == true);
+		REQUIRE(vdb.isInitialized() == true);
+	}
+}
