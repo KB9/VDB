b791933b775905924d1cffbdddab346640ccf9ab
KB9 <kavanbickerstaff@googlemail.com>
2018-04-15 06:07:49 +0100

    Added abstraction for debug info retrieval

---
+++ src/core/DebugInfo.cpp
@@ -0,0 +1,93 @@
+#include "DebugInfo.hpp"
+
+#include <cassert>
+
+#include "dwarf/DwarfExprInterpreter.hpp"
+#include "ValueDeducer.hpp"
+
+std::shared_ptr<DebugInfo> DebugInfo::readFrom(const std::string &executable_name)
+{
+	return std::make_shared<DwarfDebugInfo>(executable_name);
+}
+
+DwarfDebugInfo::DwarfDebugInfo(const std::string &executable_name) :
+	dwarf(std::make_shared<DwarfDebug>(executable_name))
+{
+
+}
+
+DwarfDebugInfo::Variable DwarfDebugInfo::getVariable(const std::string &variable_name, pid_t pid) const
+{
+	DwarfDebugInfo::Variable var;
+	var.name = variable_name;
+
+	auto loc_expr = dwarf->info()->getVarLocExpr(variable_name);
+	DwarfExprInterpreter interpreter(pid);
+	uint64_t address = interpreter.parse(&loc_expr.frame_base,
+	                                     loc_expr.location_op,
+	                                     loc_expr.location_param);
+	if (address > 0)
+	{
+		ValueDeducer deducer(pid, dwarf);
+		var.value = deducer.deduce(address, *(loc_expr.type));
+	}
+	else
+	{
+		var.value = "Variable not locatable";
+	}
+	return var;
+}
+
+DwarfDebugInfo::Function DwarfDebugInfo::getFunction(uint64_t address) const
+{
+	DIEMatcher matcher;
+	matcher.setTags({"DW_TAG_subprogram"});
+	std::vector<DIE> subprograms = dwarf->info()->getDIEs(matcher);
+	for (auto sub : subprograms)
+	{
+		uint64_t low_pc = sub.getAttributeByCode(DW_AT_low_pc).getAddress();
+		uint64_t high_pc = sub.getAttributeByCode(DW_AT_high_pc).getOffset();
+		if (address >= low_pc && address < (low_pc + high_pc))
+		{
+			DebugInfo::Function function;
+			// function.name = sub.getTagName();
+			function.name = sub.getAttributeByCode(DW_AT_name).getString();
+			function.start_address = sub.getAttributeByCode(DW_AT_low_pc).getAddress();
+			function.end_address = sub.getAttributeByCode(DW_AT_low_pc).getAddress() + sub.getAttributeByCode(DW_AT_high_pc).getOffset();
+
+			Dwarf_Off cu_offset = sub.getCUOffset();
+			DIE cu = *(dwarf->info()->getDIEByOffset(cu_offset));
+			Attribute file_name = cu.getAttributeByCode(DW_AT_name);
+			Attribute file_dir = cu.getAttributeByCode(DW_AT_comp_dir);
+			function.decl_file = file_dir.getString() + "/" + file_name.getString();
+
+			function.decl_line = sub.getAttributeByCode(DW_AT_decl_line).getUnsigned();
+
+			return function;
+		}
+	}
+
+	assert(false && "Address not within a function");
+}
+
+std::vector<DwarfDebugInfo::SourceLine> DwarfDebugInfo::getAllLines() const
+{
+	std::vector<DebugInfo::SourceLine> all_lines;
+	for (Line line : dwarf->line()->getAllLines())
+	{
+		all_lines.push_back({line.number, line.address, line.source});
+	}
+	return all_lines;
+}
+
+std::vector<std::string> DwarfDebugInfo::getSourceFiles() const
+{
+	std::vector<std::string> file_names;
+
+	std::vector<SourceFile> files = sourceFiles(dwarf);
+	for (const auto &file : files)
+	{
+		file_names.push_back(file.dir + '/' + file.name);
+	}
+	return file_names;
+}
---
+++ src/core/DebugInfo.hpp
@@ -0,0 +1,66 @@
+#ifndef _DEBUG_INFO_H_
+#define _DEBUG_INFO_H_
+
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <sys/types.h>
+
+#include "dwarf/DwarfDebug.hpp"
+
+/*
+This is a unified and simplified interface for retrieving information about
+debugging information, instead of manipulating DWARF information outside of the
+dwarf subfolder (including having to worry about the differences in information
+between DWARF versions).
+*/
+
+class DebugInfo
+{
+public:
+	struct Function
+	{
+		std::string name;
+		uint64_t start_address;
+		uint64_t end_address;
+		std::string decl_file;
+		uint64_t decl_line;
+	};
+
+	struct Variable
+	{
+		std::string name;
+		std::string value;
+	};
+
+	struct SourceLine
+	{
+		uint64_t number;
+		uint64_t address;
+		std::string file_name;
+	};
+
+	static std::shared_ptr<DebugInfo> readFrom(const std::string &executable_name);
+
+	virtual Variable getVariable(const std::string &variable_name, pid_t pid) const = 0;
+	virtual Function getFunction(uint64_t address) const = 0;
+	virtual std::vector<SourceLine> getAllLines() const = 0;
+	virtual std::vector<std::string> getSourceFiles() const = 0;
+};
+
+class DwarfDebugInfo : public DebugInfo
+{
+public:
+	DwarfDebugInfo(const std::string &executable_name);
+
+	virtual Variable getVariable(const std::string &variable_name, pid_t pid) const override;
+	virtual Function getFunction(uint64_t address) const override;
+	virtual std::vector<SourceLine> getAllLines() const override;
+	virtual std::vector<std::string> getSourceFiles() const override;
+
+private:
+	std::shared_ptr<DwarfDebug> dwarf = nullptr;
+};
+
+#endif // _DEBUG_INFO_H_
